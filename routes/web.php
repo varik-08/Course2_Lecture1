7bd7c44f2d0e8b5b02145980292f220b868a9264
varik-08 <varik-08@mail.ru>
2018-12-01 17:09:02 +0500

    1.2

--- routes/web.php
+++ routes/web.php
@@ -13,7 +13,7 @@
 Auth::routes();
 
 Route::get('/', 'UserController@index');
-Route::post('/createOrder','UserController@CreateOrder')->name('CreateOrder');
+Route::post('/create-order','UserController@CreateOrder')->name('CreateOrder');
 Route::get('/orders', 'AdminController@Orders')->name('GetOrders')->middleware('auth');
 Route::get('/products', 'AdminController@Products')->name('GetProducts')->middleware('auth');
 Route::post('/products', 'AdminController@CreateProduct')->name('CreateProduct')->middleware('auth');
